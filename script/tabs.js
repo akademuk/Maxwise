c5f07f30c361f9ccc35afe1ef28b51fbbd0d45e8
akademuk <akademuk24@gmail.com>
2022-12-16 17:01:45 +0200

    sss

--- script/tabs.js
+++ script/tabs.js
@@ -33,13 +33,13 @@
         $('.tabs li').removeClass('active');
         $(this).addClass('active');
         $('.fromAreasTabsContainer').hide().addClass("activeBlog");
-        $('.bodyOn').addClass('body');
+        // $('.bodyOn').addClass('body');
         var selectTab = $(this).find('a').attr("href");
     
         $(selectTab).fadeIn();
         $(".closeSvgBlog").click(function(){
           $(".fromAreasTabsContainer").hide();
-          $('.bodyOn').removeClass('body');
+          // $('.bodyOn').removeClass('body');
         });
       });
     });
