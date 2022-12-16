ef4c36de4e960e84c3206f18335ff505e01d3ea0
akademuk <akademuk24@gmail.com>
2022-12-16 16:17:33 +0200

    sss

--- script/tabs.js
+++ script/tabs.js
@@ -1,17 +1,46 @@
-$(document).ready(function($) {
-  $('.fromAreasTabsContainer').hide();
-  $('.fromAreasTabsContainer:first').show();
-  $('.tabs li:first').addClass('active');
-  $('.tabs li').click(function(event) {
-    $('.tabs li').removeClass('active');
-    $(this).addClass('active');
-    $('.fromAreasTabsContainer').hide().addClass("activeBlog");
 
-    var selectTab = $(this).find('a').attr("href");
-
-    $(selectTab).fadeIn();
-    $(".closeSvgBlog").click(function(){
-      $(".fromAreasTabsContainer").hide();
+  if (screen.width >= 767) {
+    $(document).ready(function($) {
+      $('.fromAreasTabsContainer').hide();
+      $('.fromAreasTabsContainer:first').show();
+      $('.tabs li:first').addClass('active');
+      $('.tabs li').click(function(event) {
+        $('.tabs li').removeClass('active');
+        $(this).addClass('active');
+        $('.fromAreasTabsContainer').hide().addClass("activeBlog");
+    
+        var selectTab = $(this).find('a').attr("href");
+    
+        $(selectTab).fadeIn();
+        $(".closeSvgBlog").click(function(){
+          $(".fromAreasTabsContainer").hide();
+        });
+      });
     });
-  });
-});
+  }
+
+
+
+
+
+
+
+  if (screen.width <= 767) {
+    $(document).ready(function($) {
+      $('.fromAreasTabsContainer').hide();
+      $('.tabs li:first').removeClass('active');
+      $('.tabs li').click(function(event) {
+        $('.tabs li').removeClass('active');
+        $(this).addClass('active');
+        $('.fromAreasTabsContainer').hide().addClass("activeBlog");
+        $('.bodyOn').addClass('body');
+        var selectTab = $(this).find('a').attr("href");
+    
+        $(selectTab).fadeIn();
+        $(".closeSvgBlog").click(function(){
+          $(".fromAreasTabsContainer").hide();
+          $('.bodyOn').removeClass('body');
+        });
+      });
+    });
+  }
