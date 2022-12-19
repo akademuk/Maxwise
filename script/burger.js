fb6c41834f2412f9bcccd6c65446849650c7f920
akademuk <akademuk24@gmail.com>
2022-12-19 18:14:49 +0200

    ss

--- script/burger.js
+++ script/burger.js
@@ -1,19 +1,4 @@
-// const burger = document.querySelector('.burger');
-// const navbar = document.querySelector('.navbar');
-// const navLink = document.querySelectorAll('.nav-link');
-// const burgerClose = document.querySelectorAll('.burgerClose');
 
-// var body = document.querySelector(".bodyOn");
-// burger.addEventListener('click', () => {
-//     navbar.classList.toggle('nav-open');
-//     burger.classList.toggle('burger-open');
-//     body.classList.add("body");
-//     navLink.forEach(link => {
-//         link.classList.toggle('nav-link-open');
-//         navbar.classList.remove('nav-open');
-//         body.classList.remove("body");
-//     })
-// });
 
 $(".burger").on("click", function(){
     $(".navbar").addClass("nav-open");
