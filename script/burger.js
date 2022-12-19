a54bc99af4370e7aa321abb3c1d8c954057ce026
akademuk <akademuk24@gmail.com>
2022-12-19 18:14:18 +0200

    sss

--- script/burger.js
+++ script/burger.js
@@ -1,18 +1,23 @@
-const burger = document.querySelector('.burger');
-const navbar = document.querySelector('.navbar');
-const navLink = document.querySelectorAll('.nav-link');
-const burgerClose = document.querySelectorAll('.burgerClose');
+// const burger = document.querySelector('.burger');
+// const navbar = document.querySelector('.navbar');
+// const navLink = document.querySelectorAll('.nav-link');
+// const burgerClose = document.querySelectorAll('.burgerClose');
 
-var body = document.querySelector(".bodyOn");
-burger.addEventListener('click', () => {
-    navbar.classList.toggle('nav-open');
-    burger.classList.toggle('burger-open');
-    body.classList.add("body");
-    navLink.forEach(link => {
-        link.classList.toggle('nav-link-open');
-        navbar.classList.remove('nav-open');
-        body.classList.remove("body");
-    })
-    
+// var body = document.querySelector(".bodyOn");
+// burger.addEventListener('click', () => {
+//     navbar.classList.toggle('nav-open');
+//     burger.classList.toggle('burger-open');
+//     body.classList.add("body");
+//     navLink.forEach(link => {
+//         link.classList.toggle('nav-link-open');
+//         navbar.classList.remove('nav-open');
+//         body.classList.remove("body");
+//     })
+// });
+
+$(".burger").on("click", function(){
+    $(".navbar").addClass("nav-open");
 });
-
+$(".burger-open").on("click", function(){
+    $(".navbar").removeClass("nav-open");
+});
