d6035ca014a8481ed8ae24cbbaf44afc122ec4c1
akademuk <akademuk24@gmail.com>
2023-01-26 12:23:42 +0200

    ss

--- script/animation.js
+++ script/animation.js
@@ -1,10 +1,14 @@
-var bear = document.querySelector(".showContainer");
+var bear = document.querySelector(".showContainerSection");
+var showContainer = document.querySelector(".showContainer");
 var body = document.querySelector(".bodyOn");
+
 setTimeout(function(){
     bear.classList.add("showContainerAnimation");
-    // body.classList.add("body");
+    showContainer.classList.add("showContainerAnimationOpacity");
+    body.classList.add("body");
 },2000)
 function svgClose() {
     bear.classList.remove("showContainerAnimation");
-    // body.classList.remove("body");
+    showContainer.classList.remove("showContainerAnimationOpacity");
+    body.classList.remove("body");
 }
